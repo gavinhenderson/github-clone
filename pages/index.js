42b810bedb3964c886389f2dbcf49e6cd5eea483
Gavin Henderson <github@gavinhenderson.co.uk>
2021-03-20 18:10:18 +0000

    add cards and colors and wire it up

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@
  * Source: https://github.com/vercel/next-learn-starter/blob/master/learn-starter/pages/index.js
  */
 
-import Head from 'next/head';
-import getTimeline from '../lib/get-timeline';
-import config from '../lib/config';
-import ActivityCard from '../components/activity-card';
-import timeStampToRelativeTime from '../lib/timestamp-to-relative-time';
+import Head from "next/head";
+import getTimeline from "../lib/get-timeline";
+import config from "../lib/config";
+import ActivityCard from "../components/activity-card";
+import timeStampToRelativeTime from "../lib/timestamp-to-relative-time";
+import getRepos from "../lib/get-repos";
+import languageColours from "../lib/colours.json";
 
 export default function Home({ feed }) {
   return (
@@ -17,18 +19,21 @@ export default function Home({ feed }) {
       </Head>
 
       <main>
-        {feed.map(({ username, userId, repoPath, activity, timeSince }) => {
-          return (
-            <ActivityCard
-              key={`${username}-${repoPath}-${activity}`}
-              username={username}
-              activity={activity}
-              repoPath={repoPath}
-              userId={userId}
-              timeSince={timeSince}
-            ></ActivityCard>
-          );
-        })}
+        {feed.map(
+          ({ username, userId, repoPath, activity, timeSince, repo }) => {
+            return (
+              <ActivityCard
+                key={`${username}-${repoPath}-${activity}`}
+                username={username}
+                activity={activity}
+                repoPath={repoPath}
+                userId={userId}
+                timeSince={timeSince}
+                repo={repo}
+              ></ActivityCard>
+            );
+          }
+        )}
       </main>
     </div>
   );
@@ -41,27 +46,67 @@ export async function getServerSideProps() {
   });
 
   const filterActivities = (activityItem) => {
-    const isWatchEvent = activityItem.type === 'WatchEvent';
-    const isStarEvent = activityItem.payload.action === 'started';
+    const isWatchEvent = activityItem.type === "WatchEvent";
+    const isStarEvent = activityItem.payload.action === "started";
     return isWatchEvent && isStarEvent;
   };
 
-  const feedAsProps = feed.filter(filterActivities).map((activityItem) => {
+  const whitelistedFeedItems = feed.filter(filterActivities);
+
+  if (whitelistedFeedItems.length !== feed.length) {
+    console.warn(
+      "You dropped some events",
+      feed.filter((activity) => !filterActivities(activity)).length
+    );
+  }
+
+  const repos = await getRepos({
+    githubToken: config.githubToken,
+    repoNames: whitelistedFeedItems.map((item) => item.repo.name),
+  });
+
+  const feedAsProps = whitelistedFeedItems.map((activityItem) => {
+    const repo = repos.find((repo) => repo.id === activityItem.repo.id);
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    const updatedAtDate = new Date(repo.updated_at);
+    const month = months[updatedAtDate.getMonth()];
+    const day = updatedAtDate.getDate();
+    const languageColour = languageColours[repo.language] || {};
+    const colour = languageColour.color || "black";
+
+    if (colour === "black") {
+      console.warn("No colour defined for: ", repo.language);
+    }
+
     return {
       username: activityItem.actor.display_login,
       userId: activityItem.actor.id,
       repoPath: activityItem.repo.name,
-      activity: 'starred',
+      activity: "starred",
       timeSince: timeStampToRelativeTime(activityItem.created_at),
+      repo: {
+        language: repo.language,
+        description: repo.description,
+        stars: repo.stargazers_count,
+        updated: `${month} ${day}`,
+        languageColour: colour,
+      },
     };
   });
-
-  if (feedAsProps.length !== feed.length) {
-    console.warn(
-      'You dropped some events',
-      feed.filter((activity) => !filterActivities(activity))
-    );
-  }
 
   return { props: { feed: feedAsProps } };
 }
